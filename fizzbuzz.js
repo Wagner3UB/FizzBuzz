c8f49ee790fe9cf5770c5ecf6478a472e9e0ba1e
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2021-07-14 11:05:14 +0200

    Update fizzbuzz.js
    versao 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,29 +1,20 @@
-// FizzBuzz
-// Divisivel por 3 => 'Fizz',
-// Divisivel por 5 => 'Buzz',
-// Divisivel por 3 e 5 => 'FizzBuzz',
-// Se não for um numero => 'Não é um número',
-// Se não for divisivel nem por 3 nem por 5 => Entrada
 
+let resultado = fizzBuzz(); //Adicionar o valor a ser verificado dentro dos ()
+console.log(resultado);
 
 function fizzBuzz(entrada){
   if  (typeof entrada !== "number") {
-    console.log("Não é um número");
     return "Não é um número";
   } else if((entrada % 3 === 0) && (entrada % 5 === 0)){
-    console.log("FizzBuzz");
     return "FizzBuzz";
   } else if(entrada % 5 === 0) {
-    console.log("Buzz");
     return "Buzz"
   } else if (entrada % 3 === 0) {
-    console.log("Fizz");
     return "Fizz"
   } else {
-    console.log(entrada);
     return entrada;
-  }
-}
+  };
+};
 
-fizzBuzz(1);
+//Conferir no console
 
